0f62f333526fe560b3f2b9081932e80f2d6879b5
Cooper Saye <coopersaye@gmail.com>
2023-02-11 13:23:48 -0500

    Initialize firebase.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.scss';
+import { firebaseConfig } from '@/util/firebaseConfig';
+import { getApps, initializeApp } from 'firebase/app';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+
+// initialize firebase
+if (!getApps().length) initializeApp(firebaseConfig);
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
---
+++ util/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-ls3eXVDkiZf5VVG0nHn66-pMBfER0ao",
+  authDomain: "meetingbrew.firebaseapp.com",
+  projectId: "meetingbrew",
+  storageBucket: "meetingbrew.appspot.com",
+  messagingSenderId: "166487103341",
+  appId: "1:166487103341:web:606633337e79527f846c64"
+};
