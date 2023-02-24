4c1fd6d2a742cdefcbea801353a35ef9d478fd5b
Cooper Saye <coopersaye@gmail.com>
2023-02-24 15:04:23 -0800

    Link new favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,11 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
         <title>MeetingBrew</title>
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
