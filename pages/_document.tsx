b347a1e6b5d3934bc14b82becdeed90d31ba2c53
Cooper Saye <coopersaye@gmail.com>
2023-02-24 21:16:18 -0800

    Link custom font.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
