8c0be05cf9fa0832f1497ffb2292d4ac5ac86ae3
Cooper Saye <coopersaye@gmail.com>
2023-02-24 23:56:45 -0800

    Get id on meeting page.

--- pages/[meetingId].tsx
+++ pages/[meetingId].tsx
@@ -1,8 +1,16 @@
+import Header from '@/components/Header';
 import styles from '@/styles/pages/MeetingPage.module.scss';
+import { useRouter } from 'next/router';
 
 export default function MeetingPage() {
+  const router = useRouter();
+
+  const { meetingId } = router.query;
+
   return (
     <div className={styles.container}>
+      <Header />
+      <h1>{meetingId}</h1>
     </div>
   );
 }
