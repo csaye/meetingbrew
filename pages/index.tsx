bb74d84acba500633558958c7cc3adc5425fc614
Cooper Saye <coopersaye@gmail.com>
2023-02-24 23:57:35 -0800

    Update index page props.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import Header from '@/components/Header';
 import styles from '@/styles/pages/Index.module.scss';
+import { Meeting } from '@/util/types';
+import { collection, doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
+import { useState } from 'react';
 
 export default function Index() {
+  const db = getFirestore();
+
+  const [title, setTitle] = useState('');
+  const [id, setId] = useState('');
+
   return (
     <div className={styles.container}>
       <h1>MeetingBrew</h1>
