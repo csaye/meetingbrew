b6fe0409b9eca140953a7b8926175d7ea8d6f8fd
Cooper Saye <coopersaye@gmail.com>
2023-02-25 19:39:08 -0800

    Get meeting data on meeting page.

--- pages/[meetingId].tsx
+++ pages/[meetingId].tsx
@@ -1,11 +1,38 @@
 import Header from '@/components/Header';
 import styles from '@/styles/pages/MeetingPage.module.scss';
+import { Meeting } from '@/util/types';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function MeetingPage() {
-  const router = useRouter();
+  const db = getFirestore();
 
+  const router = useRouter();
   const { meetingId } = router.query;
+
+  const [meeting, setMeeting] = useState<Meeting | null>();
+
+  // get meeting on start
+  useEffect(() => {
+    async function getMeeting() {
+      // handle invalid meeting id
+      if (!meetingId) return;
+      if (typeof meetingId !== 'string') {
+        setMeeting(null);
+        return;
+      }
+      // get meeting data
+      const meetingRef = doc(db, 'meetings', meetingId);
+      const meetingDoc = await getDoc(meetingRef);
+      if (!meetingDoc.exists()) {
+        setMeeting(null);
+        return;
+      }
+      setMeeting(meetingDoc.data() as Meeting);
+    }
+    getMeeting();
+  }, [meetingId, db]);
 
   return (
     <div className={styles.container}>
