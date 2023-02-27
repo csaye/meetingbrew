1bf442daf6524e289c851ce8d527c1e9f7721a25
Cooper Saye <coopersaye@gmail.com>
2023-02-27 13:16:00 -0800

    Update calendar props.

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,6 +1,37 @@
+import moment, { Moment } from 'moment-timezone';
+import { useCallback, useEffect, useState } from 'react';
 import styles from '../styles/components/Calendar.module.scss';
 
-export default function Calendar() {
+type Props = {
+  timezone: string;
+  currentTimezone: string;
+  dates: string[];
+  earliest: number;
+  latest: number;
+};
+
+type Interval = {
+  index: number;
+  moment: Moment;
+  active: boolean;
+};
+
+type Day = {
+  moment: Moment;
+  intervals: Interval[];
+};
+
+export default function Calendar(props: Props) {
+  const { timezone, currentTimezone, dates, earliest, latest } = props;
+
+  const [days, setDays] = useState<Day[]>([]);
+  const [times, setTimes] = useState<Moment[]>();
+
+  const [dragAdd, setDragAdd] = useState(true);
+  const [dragStart, setDragStart] = useState<number[] | null>(null);
+  const [dragEnd, setDragEnd] = useState<number[] | null>(null);
+  const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
+
   return (
     <div className={styles.container}>
     </div>
