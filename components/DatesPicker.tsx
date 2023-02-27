76fff456e92213ccb75ff3866fa55bb6c12e87ff
Cooper Saye <coopersaye@gmail.com>
2023-02-26 22:15:36 -0800

    Create dates picker component.

---
+++ components/DatesPicker.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/components/DatesPicker.module.scss';
+
+export default function DatesPicker() {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
