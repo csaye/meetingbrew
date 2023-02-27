09d4c176139749437e7d9e9882a7e632317a9e74
Cooper Saye <coopersaye@gmail.com>
2023-02-26 19:41:41 -0800

    Remove time range selectors.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,9 @@ import { Meeting } from '@/util/types';
 import { collection, doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 import Router from 'next/router';
 import { useState } from 'react';
-import Select from 'react-select';
 import TimezoneSelect from 'react-timezone-select';
 
 const reservedIds = ['about'];
-
-const earliestOptions = Array(24).fill(null).map((v, i) => (
-  { value: i, label: `${i % 12 || 12}${i < 12 ? 'am' : 'pm'}` })
-);
-const latestOptions = Array(24).fill(null).map((v, i) => (
-  { value: i + 1, label: `${((i + 1) % 12) || 12}${(i + 1) % 24 < 12 ? 'am' : 'pm'}` })
-);
 
 export default function Index() {
   const db = getFirestore();
@@ -75,32 +67,6 @@ export default function Index() {
             value={timezone}
             onChange={tz => setTimezone(tz.value)}
             instanceId="select-timezone"
-          />
-          <p>No earlier than</p>
-          <Select
-            value={earliest}
-            onChange={option => {
-              if (option === null) return;
-              if (option.value >= latest.value) {
-                setLatest(latestOptions[option.value]);
-              }
-              setEarliest(option);
-            }}
-            options={earliestOptions}
-            instanceId="select-earliest"
-          />
-          <p>No later than</p>
-          <Select
-            value={latest}
-            onChange={option => {
-              if (option === null) return;
-              if (option.value <= earliest.value) {
-                setEarliest(earliestOptions[option.value - 1])
-              }
-              setLatest(option);
-            }}
-            options={latestOptions}
-            instanceId="select-latest"
           />
           <input
             value={title}
