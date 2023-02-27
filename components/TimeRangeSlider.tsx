18c0e81d372d87ca955582396841289d1508298a
Cooper Saye <coopersaye@gmail.com>
2023-02-26 18:22:55 -0800

    Create time range slider component.

---
+++ components/TimeRangeSlider.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/components/TimeRangeSlider.module.scss';
+
+export default function TimeRangeSlider() {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
