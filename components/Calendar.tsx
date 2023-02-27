24698f55c337266f8a3e60348c1167f8a7ce274e
Cooper Saye <coopersaye@gmail.com>
2023-02-27 13:19:12 -0800

    Convert intervals to local timezone.

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -32,6 +32,30 @@ export default function Calendar(props: Props) {
   const [dragEnd, setDragEnd] = useState<number[] | null>(null);
   const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
 
+  // update days on info change
+  useEffect(() => {
+    const activeIntervals: string[] = [];
+    const newDates: string[] = [];
+    let index = 0;
+    let minHour, maxHour;
+    for (const date of dates) {
+      for (let hour = earliest; hour < latest; hour++) {
+        // set moment and switch timezone
+        const hourPadded = hour.toString().padStart(2, '0');
+        let mmt = moment.tz(`${date} ${hourPadded}:00:00`, timezone);
+        mmt = mmt.clone().tz(currentTimezone);
+        // update hour range
+        const hr = mmt.hour();
+        if (minHour === undefined || hr < minHour) minHour = hr;
+        if (maxHour === undefined || hr > maxHour) maxHour = hr;
+        // record date and create interval
+        const newDate = mmt.format('YYYY-MM-DD');
+        if (!newDates.includes(newDate)) newDates.push(newDate);
+        activeIntervals.push(mmt.format('YYYY-MM-DD HH:mm'));
+      }
+    }
+  }, [days, dragAdd, dragEnd, dragStart, selectedIndices]);
+
   return (
     <div className={styles.container}>
     </div>
