5549ebf4cf84fedbc510f81ed98878dc6427a81d
Cooper Saye <coopersaye@gmail.com>
2023-02-27 16:08:18 -0800

    Make intervals 15 minutes long.

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -38,20 +38,24 @@ export default function Calendar(props: Props) {
     const newDates: string[] = [];
     let index = 0;
     let minHour, maxHour;
+    // get all active intervals
     for (const date of dates) {
       for (let hour = earliest; hour < latest; hour++) {
-        // set moment and switch timezone
-        const hourPadded = hour.toString().padStart(2, '0');
-        let mmt = moment.tz(`${date} ${hourPadded}:00:00`, timezone);
-        mmt = mmt.clone().tz(currentTimezone);
-        // update hour range
-        const hr = mmt.hour();
-        if (minHour === undefined || hr < minHour) minHour = hr;
-        if (maxHour === undefined || hr > maxHour) maxHour = hr;
-        // record date and create interval
-        const newDate = mmt.format('YYYY-MM-DD');
-        if (!newDates.includes(newDate)) newDates.push(newDate);
-        activeIntervals.push(mmt.format('YYYY-MM-DD HH:mm'));
+        for (let minute = 0; minute < 60; minute += 15) {
+          // set moment and switch timezone
+          const hourPadded = hour.toString().padStart(2, '0');
+          const minutePadded = minute.toString().padStart(2, '0');
+          let mmt = moment.tz(`${date} ${hourPadded}:${minutePadded}:00`, timezone);
+          mmt = mmt.clone().tz(currentTimezone);
+          // update hour range
+          const hr = mmt.hour();
+          if (minHour === undefined || hr < minHour) minHour = hr;
+          if (maxHour === undefined || hr > maxHour) maxHour = hr;
+          // record date and create interval
+          const newDate = mmt.format('YYYY-MM-DD');
+          if (!newDates.includes(newDate)) newDates.push(newDate);
+          activeIntervals.push(mmt.format('YYYY-MM-DD HH:mm'));
+        }
       }
     }
     if (minHour === undefined || maxHour === undefined) throw 'undefined hours';
@@ -62,12 +66,15 @@ export default function Calendar(props: Props) {
       for (let hour = minHour; hour <= maxHour; hour++) {
         // skip if no matching hour
         if (!activeIntervals.some(i => parseInt(i.split(' ')[1].split(':')[0]) === hour)) continue;
-        // add interval
-        const hourPadded = hour.toString().padStart(2, '0');
-        const mmt = moment.tz(`${date} ${hourPadded}:00:00`, currentTimezone);
-        const active = activeIntervals.includes(mmt.format('YYYY-MM-DD HH:mm'));
-        ivs.push({ index: active ? index : -1, moment: mmt, active });
-        if (active) index++;
+        for (let minute = 0; minute < 60; minute += 15) {
+          // add interval
+          const hourPadded = hour.toString().padStart(2, '0');
+          const minutePadded = minute.toString().padStart(2, '0');
+          const mmt = moment.tz(`${date} ${hourPadded}:${minutePadded}:00`, currentTimezone);
+          const active = activeIntervals.includes(mmt.format('YYYY-MM-DD HH:mm'));
+          ivs.push({ index: active ? index : -1, moment: mmt, active });
+          if (active) index++;
+        }
       }
       const mmt = moment.tz(date, currentTimezone);
       newDays.push({ moment: mmt, intervals: ivs });
