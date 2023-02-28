045082a0ce87f118654f3c1985476f37fcdf2740
Brian Travis <bztravis@umich.edu>
2023-02-27 21:14:53 -0500

    Add globals color vars, small styling changes
    Add create event active color, update Outfit font weights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;600&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
