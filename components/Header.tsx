fc0cf3ec576fac144dac80b6c1a90289dbb264cb
Cooper Saye <coopersaye@gmail.com>
2023-03-01 23:49:03 -0800

    Clean up header component.

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,21 @@ import Link from 'next/link';
 import styles from '../styles/components/Header.module.scss';
 
 type Props = {
-  width: number
-}
+  width: number;
+};
 
 export default function Header(props: Props) {
+  const { width } = props;
+
   return (
     <div className={styles.container}>
-      <div className={styles.innerContainer} style={{ width: `${props.width}px` }}>
+      <div className={styles.innerContainer} style={{ width: `${width}px` }}>
         <Link href="/">
-          <Image src="/img/logo.svg" width="218" height="36" alt="logo.svg" />
+          {
+            width < 576 ?
+              <Image src="/img/logosmall.svg" width="36" height="36" alt="logosmall.svg" /> :
+              <Image src="/img/logo.svg" width="218" height="36" alt="logo.svg" />
+          }
         </Link>
         <span style={{ flexGrow: 1 }} />
         <Link className={styles.link} href="/about">About</Link>
