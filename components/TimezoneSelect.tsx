0745c23f7177e361d2367af64313b1f91b4f8ebb
Cooper Saye <coopersaye@gmail.com>
2023-03-02 15:39:25 -0800

    Create timezone select component.

---
+++ components/TimezoneSelect.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/components/TimezoneSelect.module.scss';
+
+export default function TimezoneSelect() {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
