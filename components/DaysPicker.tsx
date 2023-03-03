bff7c72cc6dcd8fa36b0e90245bd0d1c11e069f9
Cooper Saye <coopersaye@gmail.com>
2023-03-03 02:15:28 -0800

    Construct days picker.

--- components/DaysPicker.tsx
+++ components/DaysPicker.tsx
@@ -13,6 +13,40 @@ export default function DaysPicker(props: Props) {
 
   return (
     <div className={styles.container}>
+      <div className={styles.headings}>
+        {
+          ['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((d, i) =>
+            <div className={styles.heading} key={i}>
+              <h4>{d}</h4>
+            </div>
+          )
+        }
+      </div>
+      <div className={styles.days}>
+        {
+          Array(7).fill(null).map((v, i) =>
+            <div
+              onMouseDown={() => {
+                const newDays = days.slice();
+                const dayIndex = days.indexOf(i);
+                if (dayIndex === -1) newDays.push(i);
+                else newDays.splice(dayIndex, 1);
+                setDays(newDays);
+              }}
+              className={styleBuilder([
+                styles.day,
+                [styles.selected, days.includes(i)]
+              ])}
+              key={i}
+            >
+              {
+                days.includes(i) &&
+                <Image src="/icons/check.svg" width="24" height="24" alt="check.svg" />
+              }
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
