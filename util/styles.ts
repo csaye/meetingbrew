643ecd1a0c4d4243c38d33e73f35442e4a06cd5d
Cooper Saye <coopersaye@gmail.com>
2023-03-03 02:13:19 -0800

    Define select component styles.

--- util/styles.ts
+++ util/styles.ts
@@ -1,3 +1,5 @@
+import { CSSObjectWithLabel } from "react-select";
+
 // returns class name built with given style data
 export function styleBuilder(styles: (string | [string, boolean])[]) {
   const usedStyles: string[] = [];
@@ -7,3 +9,36 @@ export function styleBuilder(styles: (string | [string, boolean])[]) {
   }
   return usedStyles.join(' ');
 }
+
+// styles applied to react select component
+export const selectStyles = {
+  control: (baseStyles: CSSObjectWithLabel, state: { isFocused: boolean; }) => ({
+    ...baseStyles,
+    height: '48px',
+    borderRadius: 0,
+    background: '#f0f0f0',
+    borderWidth: '1px',
+    borderColor: state.isFocused ? 'var(--gray)' : 'var(--mid-gray)',
+    boxShadow: 'none',
+    '&:hover': {
+      borderColor: 'var(--gray)',
+    }
+  }),
+  menu: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    borderRadius: 0
+  }),
+  menuList: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    padding: 0
+  }),
+  option: (baseStyles: CSSObjectWithLabel, state: { isSelected: boolean; isFocused: boolean; }) => ({
+    ...baseStyles,
+    backgroundColor: '#fff',
+    background: state.isSelected ? 'var(--mb-red)' :
+      state.isFocused ? 'rgba(250, 56, 56, 0.2)' : undefined,
+    '&:active': {
+      background: 'rgba(250, 56, 56, 0.33)'
+    }
+  })
+};
