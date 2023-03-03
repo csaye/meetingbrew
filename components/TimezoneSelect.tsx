cf9a00cba4e618574bc37bcf453b10e6bd8cf012
Cooper Saye <coopersaye@gmail.com>
2023-03-02 23:52:41 -0800

    Construct timezone select content.

--- components/TimezoneSelect.tsx
+++ components/TimezoneSelect.tsx
@@ -11,7 +11,25 @@ export default function TimezoneSelect(props: Props) {
   const { timezone, setTimezone, className } = props;
 
   return (
-    <div className={styles.container}>
-    </div>
+    <TimezoneSelectBase
+      className={className}
+      value={timezone}
+      onChange={tz => setTimezone(tz.value)}
+      instanceId="select-timezone"
+      styles={{
+        control: baseStyles => ({
+          ...baseStyles,
+          height: '48px',
+          borderRadius: 0,
+          background: '#f0f0f0',
+          border: '1px solid #d0d0d0'
+        }),
+        singleValue: baseStyles => ({
+          ...baseStyles,
+          fontWeight: 700,
+          color: '#666'
+        })
+      }}
+    />
   );
 }
