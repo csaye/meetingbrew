b65f0a0b99e60b0c0d0bd54b7f67e529a5792b1a
Cooper Saye <coopersaye@gmail.com>
2023-03-03 02:15:22 -0800

    Update days picker props.

--- components/DaysPicker.tsx
+++ components/DaysPicker.tsx
@@ -1,6 +1,16 @@
+import { styleBuilder } from '@/util/styles';
+import Image from 'next/image';
+import { Dispatch } from 'react';
 import styles from '../styles/components/DaysPicker.module.scss';
 
-export default function DaysPicker() {
+type Props = {
+  days: number[];
+  setDays: Dispatch<number[]>;
+};
+
+export default function DaysPicker(props: Props) {
+  const { days, setDays } = props;
+
   return (
     <div className={styles.container}>
     </div>
