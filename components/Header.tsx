7ca094c49cefe91cd1bc37a6a8c51d25c7259608
Cooper Saye <coopersaye@gmail.com>
2023-03-04 15:33:30 -0800

    Hide header before width set.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Router from 'next/router';
 import styles from '../styles/components/Header.module.scss';
 
 type Props = {
@@ -11,21 +12,24 @@ export default function Header(props: Props) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.innerContainer} style={{ width: `${width}px` }}>
-        <Link href="/">
-          {
-            width < 576 ?
-              <Image src="/img/logosmall.svg" width="36" height="36" alt="logosmall.svg" /> :
-              <Image src="/img/logo.svg" width="218" height="36" alt="logo.svg" />
-          }
-        </Link>
-        <span style={{ flexGrow: 1 }} />
-        <Link className={styles.link} href="/about">About</Link>
-        <button>
-          <Image src="/icons/add.svg" width="24" height="24" alt="add.svg" />
-          New Event
-        </button>
-      </div>
+      {
+        !!width &&
+        <div className={styles.innerContainer} style={{ width }}>
+          <Link href="/">
+            {
+              width < 576 ?
+                <Image src="/img/logosmall.svg" width="36" height="36" alt="logosmall.svg" /> :
+                <Image src="/img/logo.svg" width="218" height="36" alt="logo.svg" />
+            }
+          </Link>
+          <span style={{ flexGrow: 1 }} />
+          <Link className={styles.link} href="/about">About</Link>
+          <button onClick={() => Router.push('/')}>
+            <Image src="/icons/add.svg" width="24" height="24" alt="add.svg" />
+            New Event
+          </button>
+        </div>
+      }
     </div>
   );
 }
