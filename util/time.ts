2ce0b3c67749d8e03cec3a112386a13451355bb5
Cooper Saye <coopersaye@gmail.com>
2023-03-06 23:16:05 -0500

    Extract interval time string function.

--- util/time.ts
+++ util/time.ts
@@ -1,3 +1,5 @@
+import { Interval } from '@/util/types';
+
 // returns time string for given hour and minute
 export function timeString(hour: number, minute?: number) {
   const amPm = hour < 12 || hour === 24 ? 'AM' : 'PM';
@@ -5,4 +7,14 @@ export function timeString(hour: number, minute?: number) {
   if (minute === undefined) return `${hourString} ${amPm}`;
   const minuteString = minute.toString().padStart(2, '0');
   return `${hourString}:${minuteString} ${amPm}`;
+}
+
+// returns time by interval index
+export function intervalTimeString(interval: Interval) {
+  const { hour, minute } = interval;
+  const startTime = timeString(hour, minute);
+  const nextMinute = (minute + 15) % 60;
+  const nextHour = nextMinute ? hour : (hour + 1) % 24;
+  const endTime = timeString(nextHour, nextMinute);
+  return `${startTime} – ${endTime}`;
 }
