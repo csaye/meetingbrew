684c4692bdfc8d4df147db11ce7b12124883c752
Cooper Saye <coopersaye@gmail.com>
2023-10-28 16:27:21 -0400

    Clean up about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/Header';
-import styles from '../styles/pages/About.module.scss';
+import Header from "@/components/Header";
+import styles from "../styles/pages/About.module.scss";
 
 export default function About() {
   return (
@@ -8,11 +8,16 @@ export default function About() {
       <div className={styles.outerContent}>
         <div className={styles.content}>
           <h1>MeetingBrew</h1>
-          <p>MeetingBrew is a modern scheduling tool to help you find the best communal time to meet. It was built by two computer science students at the University of Michigan. Thank you for using MeetingBrew!</p>
+          <p>
+            MeetingBrew is a modern scheduling tool to help you find the best
+            communal time to meet. It was built by two computer science students
+            at the University of Michigan. Thank you for using MeetingBrew!
+          </p>
           <div className={styles.contact}>
-            <p>Contact us:</p>
-            <li><a href="mailto:bztravis@umich.edu,csaye@umich.edu">bztravis@umich.edu</a> (design)</li>
-            <li><a href="mailto:bztravis@umich.edu,csaye@umich.edu">csaye@umich.edu</a> (development)</li>
+            <p>
+              Contact us:{" "}
+              <a href="mailto:hi@meetingbrew.com">hi@meetingbrew.com</a>
+            </p>
           </div>
         </div>
       </div>
